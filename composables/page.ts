18315d77e7ae6c0aa5ecd0a1d2f51244a1d1cb6f
Joseph Pleass <jpleass@googlemail.com>
2023-09-12 11:14:55 +0200

    Initial commit

---
+++ composables/page.ts
@@ -0,0 +1,71 @@
+import { joinURL } from 'ufo'
+
+/**
+ * Returns the currently active page, similar to Kirby's `$page` global variable
+ */
+export function usePage<T extends Record<string, any> = Record<string, any>>() {
+  return useState<T>('app.page', () => ({}) as T)
+}
+
+/**
+ * Sets the currently active page and updates the document head
+ */
+export function setPage<T extends Record<string, any>>(page?: T) {
+  const pageState = usePageState()
+
+  if (!page) {
+    pageState.value = 'rejected'
+    return
+  }
+
+  usePage().value = page
+
+  // Build the page meta tags
+  const { siteUrl } = useRuntimeConfig().public
+  const site = useSite()
+  const title = page.title
+    ? `${page.title} – ${site.value.title}`
+    : site.value.title
+  const description = page.description || site.value.description
+  const url = joinURL(siteUrl, useRoute().path)
+
+  useHead({
+    bodyAttrs: {
+      'data-template': page.intendedTemplate || 'default',
+    },
+    link: [{ rel: 'canonical', href: url }],
+  })
+
+  useSeoMeta({
+    title,
+    description,
+    ogTitle: title,
+    ogDescription: description,
+    ogUrl: url,
+    ogType: 'website',
+    twitterTitle: title,
+    twitterDescription: description,
+    twitterCard: 'summary',
+  })
+
+  pageState.value = 'resolved'
+}
+
+/**
+ * Returns a promise that resolves when the page data has been loaded or rejected
+ */
+export async function hasPage() {
+  const state = usePageState()
+
+  await until(state).not.toBe('pending')
+  await nextTick()
+
+  return state.value === 'resolved'
+}
+
+function usePageState() {
+  return useState<'pending' | 'resolved' | 'rejected'>(
+    'app.state.page',
+    () => 'pending',
+  )
+}
---
+++ nuxt.config.ts
@@ -0,0 +1,41 @@
+import { siteQuery } from './queries'
+
+export default defineNuxtConfig({
+  modules: ['@vueuse/nuxt', 'nuxt-kql'],
+
+  runtimeConfig: {
+    public: {
+      siteUrl: '',
+    },
+  },
+
+  kql: {
+    auth: 'bearer',
+    prefetch: {
+      // Currently only used to infer the type of the `site` query
+      kirbySite: siteQuery,
+    },
+    // Optionally, cache the queries on the server for an hour
+    server: {
+      // cache: true,
+      // maxAge: 60 * 60,
+    },
+  },
+
+  nitro: {
+    prerender: {
+      // Prerender the index page
+      routes: ['/'],
+    },
+  },
+
+  experimental: {
+    typescriptBundlerResolution: true,
+  },
+
+  typescript: {
+    // Recommended: type check on build
+    // typeCheck: 'build',
+    shim: false,
+  },
+})
